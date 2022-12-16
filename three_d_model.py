2be7b3a119bc6c1aa6327d27cbe93183e825e4e8
Jake Ross <jirhiker@gmail.com>
2022-12-15 17:28:47 -0700

    added raster data

--- three_d_model.py
+++ three_d_model.py
@@ -13,17 +13,31 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ===============================================================================
+import os
+import rasterio
+
 
 class FormationDB:
-    def load_rasters(self):
+    _rasters = None
+    def __init__(self, *args, **kw):
+        self._rasters = {}
 
-        for code in ['303YESO', '313SADR']:
-            p = os.path.join('data', 'rasters', f'{code}.tif')
+    def search_rasters(self, x, y, z=None):
+        root = os.path.join('data', 'discrete')
+        for pname in os.listdir(root):
+            name, ext = os.path.splitext(pname)
+            p = os.path.join(root, pname)
 
             with rasterio.open(p) as dataset:
-                self._rasters[code] = dataset
+                arr = dataset.read(1)
+                print(arr)
+
 
     def get_formation(self, x,y,z):
+        
+        self.search_rasters(x,y,z)
+
+
         return dict(code="303YESO", 
                     name="P Yeso",
                     zmin=0,
@@ -33,4 +47,10 @@ class FormationDB:
         return [dict(name="P Yeso", code="303YESO", zmin=10, zmax=20),
                 dict(name="P San Andres", code='313SADR', zmin=20, zmax=1000)]
                 
+
+
+if __name__ == '__main__':
+    fm = FormationDB()
+    fm.get_formation(0,0,10)
+
 # ============= EOF =============================================
